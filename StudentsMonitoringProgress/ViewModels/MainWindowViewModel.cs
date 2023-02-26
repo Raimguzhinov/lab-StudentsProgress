ec1577f1712ee4bdef73418bb9aa0ef655e7be21
Raimguzhinov <d.raimguzhinov@stud.sibguti.ru>
2023-02-26 17:35:34 +0700

    Add new functionality for adding/deleting students

--- StudentsMonitoringProgress/ViewModels/MainWindowViewModel.cs
+++ StudentsMonitoringProgress/ViewModels/MainWindowViewModel.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Globalization;
+using System.Reactive;
+using ReactiveUI;
 using StudentsMonitoringProgress.Models;
 
 namespace StudentsMonitoringProgress.ViewModels
 {
-    public class MainWindowViewModel : INotifyPropertyChanged
+    public class MainWindowViewModel : ReactiveObject, INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler? PropertyChanged;
-
+        public new event PropertyChangedEventHandler? PropertyChanged;
         private ObservableCollection<Student> _students = null!;
+        private int _index;
+        private string _newStudentName = null!;
+        private int _selectedVisualProgGrade;
+        private int _selectedMathAnalysisGrade;
+        private int _selectedElectrotechnicGrade;
+        private int _selectedComputerMathGrade;
+        private int _selectedPhysicalSportGrade;
+        private int _selectedArchitectureGrade;
+        private int _selectedNetworksGrade;
+        private List<int> _gradeChoices = new List<int> { 0, 1, 2 };
         public ObservableCollection<Student> Students
         {
             get { return _students; }
@@ -21,22 +32,11 @@ namespace StudentsMonitoringProgress.ViewModels
                 OnPropertyChanged("Students");
             }
         }
-
-        private int _index;
-        public int Index
-        {
-            get { return _index; }
-            set
-            {
-                _index = value;
-                OnPropertyChanged("Index");
-            }
-        }
-
         public MainWindowViewModel()
         {
             Students = new ObservableCollection<Student>();
-
+            AddStudentCommand = ReactiveCommand.Create(AddStudent);
+            DeleteStudentCommand = ReactiveCommand.Create(DeleteStudent);
             // заполнение случайными данными
             Random rnd = new Random();
             string[] names = { "Максим Назипов", "Владислав Перчун", "Константин Ефремов", "Диас Раймгужинов", "Даниил Шлипев", "Сергей Малеев" };
@@ -47,7 +47,6 @@ namespace StudentsMonitoringProgress.ViewModels
                 int index = rnd.Next(availableNames.Count); // выбираем случайный индекс из оставшихся имен
                 string name = availableNames[index]; // выбираем случайное имя из оставшихся имен
                 availableNames.Remove(name); // удаляем имя из списка доступных имен
-            
                 Student student = new Student();
                 student.Name = name;
                 student.VisualProg = rnd.Next(0, 2);
@@ -73,8 +72,7 @@ namespace StudentsMonitoringProgress.ViewModels
                 sumArchitecture += student.Architecture;
                 sumNetworks += student.Networks;
             }
-
-// Вычисляем общий средний балл по всем студентам
+            // Вычисляем общий средний балл по всем студентам
             int count = Students.Count;
             double averageVisualProg = Math.Round(sumVisualProg / count, 2);
             double averageMathAnalysis = Math.Round(sumMathAnalysis / count, 2);
@@ -83,8 +81,7 @@ namespace StudentsMonitoringProgress.ViewModels
             double averagePhysicalSport = Math.Round(sumPhysicalSport / count, 2);
             double averageArchitecture = Math.Round(sumArchitecture / count, 2);
             double averageNetworks = Math.Round(sumNetworks / count, 2);
-
-// Устанавливаем значения свойств для отображения в интерфейсе
+            // Устанавливаем значения свойств для отображения в интерфейсе
             ScVisualProg = averageVisualProg.ToString(CultureInfo.CurrentCulture);
             ScMathAnalysis = averageMathAnalysis.ToString(CultureInfo.CurrentCulture);
             ScElectrotechnic = averageElectrotechnic.ToString(CultureInfo.CurrentCulture);
@@ -93,7 +90,30 @@ namespace StudentsMonitoringProgress.ViewModels
             ScArchitecture = averageArchitecture.ToString(CultureInfo.CurrentCulture);
             ScNetworks = averageNetworks.ToString(CultureInfo.CurrentCulture);
             ScAverageMark = Math.Round((averageVisualProg + averageMathAnalysis + averageElectrotechnic + averageComputerMath + averagePhysicalSport + averageArchitecture + averageNetworks) / 7, 2).ToString(CultureInfo.CurrentCulture);
-
+        }
+        public ReactiveCommand<Unit, Unit> AddStudentCommand { get; }
+        public ReactiveCommand<Unit, Unit> DeleteStudentCommand { get; }
+        private void AddStudent()
+        {
+            Student newStudent = new Student()
+            {
+                Name = NewStudentName,
+                VisualProg = SelectedVisualProgGrade,
+                MathAnalysis = SelectedMathAnalysisGrade,
+                Electrotechnic = SelectedElectrotechnicGrade,
+                ComputerMath = SelectedComputerMathGrade,
+                PhysicalSport = SelectedPhysicalSportGrade,
+                Architecture = SelectedArchitectureGrade,
+                Networks = SelectedNetworksGrade
+            };
+            Students.Add(newStudent);
+            OnPropertyChanged(nameof(Students));
+        }
+        private void DeleteStudent()
+        {
+            Student selectedStudent = Students[Index];
+            Students.Remove(selectedStudent);
+            OnPropertyChanged(nameof(Students));
         }
         public string ScAverageMark { get; set; }
         public string ScNetworks { get; set; }
@@ -103,10 +123,65 @@ namespace StudentsMonitoringProgress.ViewModels
         public string ScElectrotechnic { get; set; }
         public string ScMathAnalysis { get; set; }
         public string ScVisualProg { get; set; }
+        public int Index
+        {
+            get { return _index; }
+            set
+            {
+                _index = value;
+                OnPropertyChanged("Index");
+            }
+        }
+        public string NewStudentName
+        {
+            get { return _newStudentName; }
+            set { this.RaiseAndSetIfChanged(ref _newStudentName, value); }
+        }
+        public int SelectedVisualProgGrade
+        {
+            get { return _selectedVisualProgGrade; }
+            set { this.RaiseAndSetIfChanged(ref _selectedVisualProgGrade, value); }
+        }
+        public int SelectedMathAnalysisGrade
+        {
+            get { return _selectedMathAnalysisGrade; }
+            set { this.RaiseAndSetIfChanged(ref _selectedMathAnalysisGrade, value); }
+        }
+        public int SelectedElectrotechnicGrade
+        {
+            get { return _selectedElectrotechnicGrade; }
+            set { this.RaiseAndSetIfChanged(ref _selectedElectrotechnicGrade, value); }
+        }
+        public int SelectedComputerMathGrade
+        {
+            get { return _selectedComputerMathGrade; }
+            set { this.RaiseAndSetIfChanged(ref _selectedComputerMathGrade, value); }
+        }
+        public int SelectedPhysicalSportGrade
+        {
+            get { return _selectedPhysicalSportGrade; }
+            set { this.RaiseAndSetIfChanged(ref _selectedPhysicalSportGrade, value); }
+        }
+        public int SelectedArchitectureGrade
+        {
+            get { return _selectedArchitectureGrade; }
+            set { this.RaiseAndSetIfChanged(ref _selectedArchitectureGrade, value); }
+        }
+        public int SelectedNetworksGrade
+        {
+            get { return _selectedNetworksGrade; }
+            set { this.RaiseAndSetIfChanged(ref _selectedNetworksGrade, value); }
+        }
+        public List<int> GradeChoices
+        {
+            get => _gradeChoices;
+            set => this.RaiseAndSetIfChanged(ref _gradeChoices, value);
+        }
         protected void OnPropertyChanged(string propName)
         {
             if (PropertyChanged != null)
                 PropertyChanged(this, new PropertyChangedEventArgs(propName));
         }
     }
+
 }
