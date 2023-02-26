4b1c836a719b4e5832c5d6d414baa944731f18a3
Raimguzhinov <d.raimguzhinov@stud.sibguti.ru>
2023-02-26 13:36:57 +0700

    3-rd point of the terms of reference are done

--- StudentsMonitoringProgress/Models/Student.cs
+++ StudentsMonitoringProgress/Models/Student.cs
@@ -5,10 +5,10 @@ namespace StudentsMonitoringProgress.Models
         public string Name { get; set; } = null!;
         public int VisualProg { get; set; }
         public int MathAnalysis { get; set; }
-        public int EES { get; set; }
+        public int Electrotechnic { get; set; }
         public int ComputerMath { get; set; }
         public int PhysicalSport { get; set; }
-        public int AEVM { get; set; }
+        public int Architecture { get; set; }
         public int Networks { get; set; }
         public double AverageMark { get; set; }
     }
--- StudentsMonitoringProgress/ViewModels/MainWindowViewModel.cs
+++ StudentsMonitoringProgress/ViewModels/MainWindowViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Globalization;
 using StudentsMonitoringProgress.Models;
 
 namespace StudentsMonitoringProgress.ViewModels
@@ -51,16 +52,57 @@ namespace StudentsMonitoringProgress.ViewModels
                 student.Name = name;
                 student.VisualProg = rnd.Next(0, 2);
                 student.MathAnalysis = rnd.Next(1, 3);
-                student.EES = rnd.Next(2, 3);
+                student.Electrotechnic = rnd.Next(2, 3);
                 student.ComputerMath = rnd.Next(0, 2);
                 student.PhysicalSport = rnd.Next(1, 3);
-                student.AEVM = rnd.Next(1, 3);
+                student.Architecture = rnd.Next(1, 3);
                 student.Networks = rnd.Next(0, 2);
-                student.AverageMark = Math.Round((double)(student.MathAnalysis + student.EES + student.ComputerMath +
-                                                           student.PhysicalSport + student.AEVM + student.Networks) / 6, 2);
+                student.AverageMark = Math.Round((double)(student.MathAnalysis + student.Electrotechnic + student.ComputerMath +
+                                                           student.PhysicalSport + student.Architecture + student.Networks) / 6, 2);
                 Students.Add(student);
             }
+            // Вычисляем средний балл для каждого предмета
+            double sumVisualProg = 0, sumMathAnalysis = 0, sumElectrotechnic = 0, sumComputerMath = 0, sumPhysicalSport = 0, sumArchitecture = 0, sumNetworks = 0;
+            foreach (var student in Students)
+            {
+                sumVisualProg += student.VisualProg;
+                sumMathAnalysis += student.MathAnalysis;
+                sumElectrotechnic += student.Electrotechnic;
+                sumComputerMath += student.ComputerMath;
+                sumPhysicalSport += student.PhysicalSport;
+                sumArchitecture += student.Architecture;
+                sumNetworks += student.Networks;
+            }
+
+// Вычисляем общий средний балл по всем студентам
+            int count = Students.Count;
+            double averageVisualProg = Math.Round(sumVisualProg / count, 2);
+            double averageMathAnalysis = Math.Round(sumMathAnalysis / count, 2);
+            double averageElectrotechnic = Math.Round(sumElectrotechnic / count, 2);
+            double averageComputerMath = Math.Round(sumComputerMath / count, 2);
+            double averagePhysicalSport = Math.Round(sumPhysicalSport / count, 2);
+            double averageArchitecture = Math.Round(sumArchitecture / count, 2);
+            double averageNetworks = Math.Round(sumNetworks / count, 2);
+
+// Устанавливаем значения свойств для отображения в интерфейсе
+            ScVisualProg = averageVisualProg.ToString(CultureInfo.CurrentCulture);
+            ScMathAnalysis = averageMathAnalysis.ToString(CultureInfo.CurrentCulture);
+            ScElectrotechnic = averageElectrotechnic.ToString(CultureInfo.CurrentCulture);
+            ScComputerMath = averageComputerMath.ToString(CultureInfo.CurrentCulture);
+            ScPhysicalSport = averagePhysicalSport.ToString(CultureInfo.CurrentCulture);
+            ScArchitecture = averageArchitecture.ToString(CultureInfo.CurrentCulture);
+            ScNetworks = averageNetworks.ToString(CultureInfo.CurrentCulture);
+            ScAverageMark = Math.Round((averageVisualProg + averageMathAnalysis + averageElectrotechnic + averageComputerMath + averagePhysicalSport + averageArchitecture + averageNetworks) / 7, 2).ToString(CultureInfo.CurrentCulture);
+
         }
+        public string ScAverageMark { get; set; }
+        public string ScNetworks { get; set; }
+        public string ScArchitecture { get; set; }
+        public string ScPhysicalSport { get; set; }
+        public string ScComputerMath { get; set; }
+        public string ScElectrotechnic { get; set; }
+        public string ScMathAnalysis { get; set; }
+        public string ScVisualProg { get; set; }
         protected void OnPropertyChanged(string propName)
         {
             if (PropertyChanged != null)
